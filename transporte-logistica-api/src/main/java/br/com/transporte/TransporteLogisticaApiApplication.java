5f039d8190bfdbc448cf66d4ddadda85e9a1b042
Gabriel Teixeira <gabrielpires011@hotmail.com>
2023-04-27 11:02:07 -0300

    Início

---
+++ transporte-logistica-api/src/main/java/br/com/transporte/TransporteLogisticaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.transporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransporteLogisticaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransporteLogisticaApiApplication.class, args);
+    }
+
+}
---
+++ transporte-logistica-api/src/test/java/br/com/transporte/TransporteLogisticaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.transporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransporteLogisticaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
